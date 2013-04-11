3f2d262f9931cba4780825843abc9f7a41f79626
aplicatii romanesti <aplicatii.romanesti@gmail.com>
2013-04-11 23:24:51 +0300

    diabled submeus like bySeries dupa titlu by_Tag

--- src/org/geometerplus/fbreader/library/Library.java
+++ src/org/geometerplus/fbreader/library/Library.java
@@ -66,10 +66,10 @@ public final class Library extends AbstractLibrary {
 		new FileFirstLevelTree(myRootTree, ROOT_FILE_TREE);//maryhit - I copied from below here as well for tests
 		new FavoritesTree(myRootTree, ROOT_FAVORITES);
 		new FirstLevelTree(myRootTree, ROOT_RECENT);
-		new FirstLevelTree(myRootTree, ROOT_BY_AUTHOR);
-		new FirstLevelTree(myRootTree, ROOT_BY_TITLE);
-		new FirstLevelTree(myRootTree, ROOT_BY_TAG);
-		new FileFirstLevelTree(myRootTree, ROOT_FILE_TREE);
+		//new FirstLevelTree(myRootTree, ROOT_BY_AUTHOR);//maryhit commented out
+		//new FirstLevelTree(myRootTree, ROOT_BY_TITLE);
+		//new FirstLevelTree(myRootTree, ROOT_BY_TAG);//maryhit commented out
+		//new FileFirstLevelTree(myRootTree, ROOT_FILE_TREE);//maryhit commented out
 	}
 
 	public LibraryTree getRootTree() {
